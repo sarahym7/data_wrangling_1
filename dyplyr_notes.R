a96793931dae7232f3587a8ae05c6bb4d0953069
Sarahy <sjm2284@cumc.columbia.edul>
2024-09-25 01:13:42 -0400

    pivot_longer example

---
+++ dyplyr_notes.R
@@ -0,0 +1,20 @@
+#dplyr package will always be coming from a dataframe first, select and filter a data.frame
+#piping is connecting multiple steps 
+#piping is a sequence of actions 
+
+
+# ex. happy_jeff = do_ds(brush_teeth(wake_up(asleep_jeff)))
+# awake_jeff = =wakeup(asleep_jeff)
+# clean_teeth_jeff = brush_teeth(awake_jeff)
+# happy_jeff = do_ds(clean_teeth_jeff)
+
+#Although we can see that they are building off of eachother its more complicated to read
+
+#In this class we will learn 
+
+#happy_jeff =
+#wake_up(asleep_jeff) %>%
+#brush_teeth() %>%
+#do_ds
+
+#works well with tibble in and tibble out dyplr function for what comes in and out of the dataframe
